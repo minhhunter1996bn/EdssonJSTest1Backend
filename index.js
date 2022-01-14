c44c4d570fc2de347116819b6423005bd974c7ed
Jackson <nhhminh@cmcglobal.vn>
2022-01-14 18:34:47 +0700

    done

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const http = require("http");
 (async () => {
   const app = await require('./server')();
   const server = http.createServer(app);
-  server.listen(3000, () => console.log('Running'));
+  server.listen(3001, () => console.log('Running'));
 })();
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const service = require('./modules/service');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 module.exports = async () => {
   const app = express();
+  app.use(cors());
   const api = require('./api')(express, service);
   app.use(
     bodyParser.json({
